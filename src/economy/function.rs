f708586020269dc4b2813e01276e1eb8cc73012d
Dawid Sula <dawidsula26@gmail.com>
2022-04-30 00:05:04 +0200

    Added function files

---
+++ src/economy/function.rs
@@ -0,0 +1,11 @@
+/// Base representation of mathematical function.
+mod base_function;
+
+/// Demand function
+mod demand;
+
+/// Supply function
+mod supply;
+
+/// Difference between supply and demand.
+mod oversupply;
