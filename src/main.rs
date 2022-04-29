b1de1530803f510d33c3ce90eda18a6b36c6fef1
Dawid Sula <dawidsula26@gmail.com>
2022-04-29 23:53:16 +0200

    Added economy modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+/// Market simulation tools
+/// 
+/// Module conatians elements used to represent economy and tools to analize its behaviour.
+mod economy;
+
 fn main() {
     println!("Hello, world!");
 }
