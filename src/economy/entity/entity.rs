d280edf3e386d1853794db29b244a6e385766b0a
Dawid Sula <dawidsula26@gmail.com>
2022-04-30 14:36:56 +0200

    Improved algorithm descriptions

--- src/economy/entity/entity.rs
+++ src/economy/entity/entity.rs
@@ -1,14 +1,24 @@
 //! Representation of generic market entity.
 //! 
-//! Basic algorithm:
-//! Producer creates goods untill price is equal to marginal costs. Only r
-//! equires one iteration of the algorithm.
+//! ## Basic algorithm:
 //! 
-//! Regular algorithm: 
+//! Producer creates goods untill price is equal to marginal costs. Requires 
+//! only one iteration of the algorithm.
+//! 
+//! ## Regular algorithm: 
+//! 
 //! Producer produces according to basic algorithm then in each iteration he
 //! corrects his production based on marginal costs. Single producer can only
-//! directly supply one node in the graph.
+//! directly supply only one node in the graph.
 //! 
-//! Advanced algorithm:
-//! Like regular algorithm, but graphs of edges where goods are transported
-//! are used to allow supplying multiple nodes.
+//! ## Advanced algorithm:
+//! 
+//! Like regular algorithm, but we allow supplying multiple nodes.
+//! Producer separates nodes they supply into groups where prices are strongly
+//! connected. They reduce sales in different groups as long as it's beneficial
+//! to them. Changing prices can cause changes to where goods are transported
+//! therefore events for each possible change are created and they are handled
+//! when they appear.
+//! 
+//! Note: This description is still unclear but it's enough for me to know
+//! what to do.
--- src/economy/market.rs
+++ src/economy/market.rs
@@ -4,6 +4,37 @@
 //! goods flow, underlying infrastructure. Includes algorithms used to
 //! calculate prices.
 //! 
-//! Price resolving algorith abstract:
+//! ## Algorithm
+//! 
+//! ### Abstract
+//! 
 //! Special graph is generated then max value flow is computed. Computed flow 
-//! represents used consumption, demand and goods transfers.
+//! represents used consumption, demand and goods transfers. Then consumers and
+//! producers update their prices and algorithm repeats.
+//! 
+//! 
+//! ### Full
+//! 
+//! At the beggining all entities calculate their supply/demand as if they did
+//! not have any market power. Then algorithms consists of multiple rounds of
+//! calculating new prices and entities updating their supply/demand.
+//! 
+//! #### Calculating prices
+//! 
+//! Special directed graph, where each edge has it's capacity and value, is
+//! constructed:
+//! 1. Graph of city connections is taken. Each edge has value equal to 
+//!     minus transport cost.
+//! 2. Node is added for each entity. 
+//! 3. Edges from entity nodes to cities are added in such a way that they 
+//!     represent their costs/willingness to pay.
+//! Then maximum value flow is found. Flow on edge from city to entity 
+//! corresponds to their production/consumption and flow on city to city edge
+//! represents transport between towns.
+//! 
+//! #### Ending algorithm
+//! 
+//! Each entity is expected to lower their demand/supply untill they reach
+//! optimal value. Therefore algorithm should stop once changes in prices
+//! are small enough.
+//! 
